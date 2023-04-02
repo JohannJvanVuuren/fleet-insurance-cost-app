089c9bf198677e19593e69a2e00b37ca79bb1dcd
Johann Jansen van Vuuren <74132512+JohannJvanVuuren@users.noreply.github.com>
2023-04-02 11:23:00 +0200

    Setup basic Expresss app.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
+/* Importing the Express module required in this route */
 const express = require('express');
+
+/* Creation of a Router instance */
 const router = express.Router();
 
-/* GET home page. */
+/* Definition of the GET home page route */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Exporting the router from this module so that it can be used and imported from other modules */
 module.exports = router;
--- test/users.test.js
+++ test/users.test.js
@@ -1,12 +1,21 @@
+/* Import of the chai expect assertion style */
 const expect = require('chai').expect;
+
+/* Import of the request package that allows the testing of HTTP requests by making HTTP calls */
 const request = require('request');
+
+/* Import of the built-in assert() method used at the bottom */
 const assert = require('assert');
 
+/* Mocha test to ensure the default functionality created for http://localhost:300/users work as expected */
+/* The describe() functions to group tests together and describes what is being tested*/
 describe('Status and content', function() {
     describe ('Users page', function() {
+        /* The it() functions describe the actual test and contains the code needed to execute the test */
         it('status', function(done){
             request('http://localhost:3000/users',
                 function(error, response, body) {
+                    /* Chai's expect assertion style used together with assert() below */
                     expect(response.statusCode).to.equal(200);
                     done();
                 });
@@ -21,4 +30,5 @@ describe('Status and content', function() {
     });
 });
 
-assert(5 > 7)
+/* Using the built-in assert() module to describe the expected outcome to be */
+assert(5 < 7);
